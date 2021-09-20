734f38727e9bc0972b2e7108130a2b2c5ab2106a
confusion-del <breezeian64@gmail.com>
2021-09-20 13:12:50 +0300

    first commit

---
+++ app/src/main/java/com/example/mylikewhatsappapplication/StatusFragment.kt
@@ -0,0 +1,29 @@
+package com.example.mylikewhatsappapplication
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class StatusFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = StatusFragment()
+
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var root = inflater.inflate(R.layout.status_fragment, container, false)
+        return root
+
+    }
+
+}
+
+//launch a file picker select an image and display it on an image view
